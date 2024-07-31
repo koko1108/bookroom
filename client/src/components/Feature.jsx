2760baf0b4035b3485ac85e083c4400a1ba435d2
koko1108 <vupzj62k@gmail.com>
2024-07-31 22:40:35 +0800

    串接hotels api 並設定proxy

--- client/src/components/Feature.jsx
+++ client/src/components/Feature.jsx
@@ -3,9 +3,13 @@ import Categories from '../subcomponents/Categories'
 import PostCards from '../subcomponents/PostCards'
 import PopularHotels from '../subcomponents/PopularHotels'
 import { CategoriesCities, CategoriesType, PopularHotelsData} from '../data'
+import useFetch from '../hooks/useFetch'
 import "./feature.scss"
 const Feature = () => {
+    const {data,loading,error} =useFetch("/hotels")
     
+    const typeUrl=`/hotels/amountoftype?type=${CategoriesType.map((type)=>type.name)}`
+    const citiesUrl=`/hotels/amountofcities?cities=${CategoriesCities.map((city)=>city.name)}`
     return (
         <div className='feature'>
             <div className="container">
@@ -13,7 +17,7 @@ const Feature = () => {
                     <h2>依住宿類型瀏覽</h2>
                 </div>
                 <div className="listItems">
-                    <Categories dataArray={CategoriesType} />
+                    <Categories dataArray={CategoriesType} url={typeUrl} />
                 </div>
                 <div className="listItems">
                     <PostCards/>
@@ -23,13 +27,13 @@ const Feature = () => {
                     <p>這些熱門目的地魅力無窮，等你來體驗！</p>
                 </div>
                 <div className="listItems">
-                    <Categories dataArray={CategoriesCities}/>
+                    <Categories dataArray={CategoriesCities} url={citiesUrl} />
                 </div>
                 <div className="listTitle">
                     <h2>人氣民宿、公寓類型住宿</h2>
                 </div>
                 <div className="listItems">
-                    <PopularHotels dataArray={PopularHotelsData}/>
+                <PopularHotels dataArray={data}/>
                 </div>
             </div>
         </div>
---
+++ client/src/hooks/useFetch.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+// useFetch統一處理axios.get資料的api
+const useFetch = (url) => {
+  const [data, setData] = useState([]); //取得的資料
+  const [loading, setLoading] = useState(false); //紀錄連線狀態，方便後的載入畫面呈現
+  const [error, setError] = useState(""); //回報錯誤
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(url);
+        setData(response.data);
+      } catch (err) {
+        setError(err);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+  return { data, loading, error };
+};
+
+export default useFetch;
